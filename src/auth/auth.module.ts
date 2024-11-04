40ad43314ec3c31335809743a546f99002d3ab23
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2024-11-04 18:18:08 -0300

    chore: change token duration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: 'your_jwt_secret',
-      signOptions: { expiresIn: '60m' },
+      signOptions: { expiresIn: '7d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
